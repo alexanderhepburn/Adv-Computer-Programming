7f786f0bd5fa815a4ae8f7a3de64f9066b2b1e3a
alexanderhepburn <zander@hepburn.com>
2024-04-21 12:53:34 +0200

    Fehler bei sum() korrigiert

--- Skill 7/Task24.py
+++ Skill 7/Task24.py
@@ -9,7 +9,7 @@
 ## Input: a, b, c all ints
 ## Returns: Int
 def sumofthree(a, b, c):
-    return sum(a, b, c)
+    return sum([a, b, c])
 
 # Print the output
 print(f"The result of the function: sumofthree(5,3,10) is: {sumofthree(5,3,10)}")
